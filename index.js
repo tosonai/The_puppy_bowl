f06243be9d0a07fee81a7546ab2f11dd4de23d02
Temitayo Sonaike <127345820+tosonai@users.noreply.github.com>
2023-05-22 19:09:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-import { fetchAllPlayers } from './ajaxHelpers'
-import { renderAllPlayers, renderNewPlayerForm } from './renderHelpers'
-
-const init = async () => {
-  const players = await fetchAllPlayers()
-  renderAllPlayers(players)
-
-  renderNewPlayerForm()
-}
-
-init()
+// Fetch and render initial player list
+function fetchPlayers() {
+    fetch('https://api.example.com/puppies')
+      .then(response => response.json())
+      .then(players => renderPlayerList(players))
+      .catch(error => console.error(error));
+  }
+  
+  function renderPlayerList(players) {
+    const playerList = document.getElementById('player-list');
+    playerList.innerHTML = '';
+  
+    players.forEach(player => {
+      const li = document.createElement('li');
+      li.textContent = player.name;
+      li.addEventListener('click', () => showPlayerDetails(player));
+      playerList.appendChild(li);
+    });
+  }
+  
+  function showPlayerDetails(player) {
+    const playerInfo = document.getElementById('player-info');
+    playerInfo.innerHTML = `
+      <h3>${player.name}</h3>
+      <p>Breed: ${player.breed}</p>
+      <p>Age: ${player.age}</p>
+      <p>Weight: ${player.weight}</p>
+      <!-- Add more details as needed -->
+    `;
+  }
+  
+  function addPlayer(event) {
+    event.preventDefault();
+    
+    const nameInput = document.getElementById('name');
+    const breedInput = document.getElementById('breed');
+  
+    const player = {
+      name: nameInput.value,
+      breed: breedInput.value
+      // Add more properties as needed
+    };
+  
+    // Here you can make a POST request to the API to add the player
+    // Once the request is successful, you can update the UI with the new player
+  
+    nameInput.value = '';
+    breedInput.value = '';
+  }
+  
+  // Event listeners
+  document.getElementById('add-player-form').addEventListener('submit', addPlayer);
+  
+  // Fetch players on page load
+  fetchPlayers();
+  
